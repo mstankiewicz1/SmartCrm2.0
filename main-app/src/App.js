5698b2d8ee3b83ec2fb1bd041f10dc631f0517f0
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2019-11-08 00:14:00 +0100

    Postepy 08.11.19r godz 00:13

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import './App.css';
+import UsersList from './UsersList';
 
-function App() {
-  return (
-    <div>
-        SmartCrm2.0 dziala
-    </div>
-  );
+
+
+const allUsers = ['Marek', 'Ania', 'Joanna', 'Bartosz', 'Michał'];
+
+class App extends React.Component {
+
+
+
+    state = {
+      filteredUsers: allUsers
+    };
+
+
+
+    filterUsers = (e) => {
+        const text = e.target.value;
+        const filteredUsers = this.getFilteredUsersForText(text);
+        this.setState({
+            filteredUsers
+        })
+    };
+
+    getFilteredUsersForText = (text) => {
+        return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()));
+    };
+
+
+    render() {
+        return (
+            <div>
+                <h1>SmartCrm2.0 działa !</h1>
+                <input onChange={this.filterUsers}/>
+                <UsersList users={this.state.filteredUsers}/>
+            </div>
+        )
+    }
 }
+
 
 export default App;
---
+++ main-app/src/UsersList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const UsersList = (props) => {
+
+
+    if (props.users.length > 0) {
+        return (
+            <div>
+                {props.users.map( user =>
+                    <li key={user}>
+                        {user}
+                    </li>
+                )}
+            </div>
+        )
+    } else {
+        return (
+            <p>Brak wyników szukania !</p>
+        )
+    }
+};
+
+export default UsersList;
