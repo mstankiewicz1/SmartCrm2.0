e1d75430dbf2834888fc1ccc2e90b1f007a65c83
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2019-11-11 22:21:06 +0100

    Postepy 11.11.19r godz 22:21

--- main-app/src/AddUser.js
+++ main-app/src/AddUser.js
@@ -14,27 +14,6 @@ class AddUser extends React.Component {
         }
     };
 
-    handleChange = (e) => {
-
-        let newUser;
-
-        newUser = {
-            ...this.state.user,
-            [e.target.name]: e.target.value
-        };
-        this.setState({
-            user: newUser
-        })
-    };
-
-    addNewUser = (e) => {
-
-        e.preventDefault();
-
-
-    };
-
-
 
 
 
@@ -42,19 +21,13 @@ class AddUser extends React.Component {
         return (
             <div className="addUserForm">
                 <form onSubmit={this.addNewUser}>
-                    <input className="name" type="text" placeholder="Wpisz imię" value={this.state.user.name}
-                           onChange={this.handleChange}/>
-                    <input className="surname" type="text" placeholder="Wpisz nazwisko" value={this.state.user.surname}
-                           onChange={this.handleChange}/>
-                    <input className="pesel" type="number" placeholder="Wpisz pesel" value={this.state.user.pesel}
-                           onChange={this.handleChange}/>
-                    <input className="mobilePhone" type="number" placeholder="Wpisz numer telefonu komórkowego" value={this.state.user.mobilePhone}
-                           onChange={this.handleChange}/>
-                    <input className="landlinePhone" type="number" placeholder="Wpisz numer telefonu stacjonarnego" value={this.state.user.landlinePhone}
-                           onChange={this.handleChange}/>
-                    <input className="email" type="email" placeholder="Wpisz adres email" value={this.state.user.email}
-                           onChange={this.handleChange}/>
-                    <button>Dodaj</button>
+                    <input className="name" type="text" placeholder="Wpisz imię" value={this.state.user.name}/>
+                    <input className="surname" type="text" placeholder="Wpisz nazwisko" value={this.state.user.surname}/>
+                    <input className="pesel" type="number" placeholder="Wpisz pesel" value={this.state.user.pesel}/>
+                    <input className="mobilePhone" type="number" placeholder="Wpisz numer telefonu komórkowego" value={this.state.user.mobilePhone}/>
+                    <input className="landlinePhone" type="number" placeholder="Wpisz numer telefonu stacjonarnego" value={this.state.user.landlinePhone}/>
+                    <input className="email" type="email" placeholder="Wpisz adres email" value={this.state.user.email}/>
+                    <button type="submit">Dodaj</button>
                 </form>
             </div>
         )
--- main-app/src/App.js
+++ main-app/src/App.js
@@ -136,6 +136,7 @@ class App extends React.Component {
     };
 
 
+
     render() {
         return (
             <div className="app">
