3f0ba06b940ed3f09ac15dff0f385ada2302ecb2
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2019-11-11 21:49:50 +0100

    Postepy 11.11.19r godz 21:49

--- main-app/src/AddUser.js
+++ main-app/src/AddUser.js
@@ -4,7 +4,27 @@ import React from 'react';
 class AddUser extends React.Component {
 
     state = {
+        user: {
+            name: "",
+            surname: "",
+            pesel: "",
+            mobilePhone: "",
+            landlinePhone: "",
+            email: ""
+        }
+    };
 
+    handleChange = (e) => {
+
+        let newUser;
+
+        newUser = {
+            ...this.state.user,
+            [e.target.name]: e.target.value
+        };
+        this.setState({
+            user: newUser
+        })
     };
 
 
@@ -13,7 +33,21 @@ class AddUser extends React.Component {
 
     render(){
         return (
-            <div>
+            <div className="addUserForm">
+                <form>
+                    <input className="name" type="text" placeholder="Wpisz imię" value={this.state.user.name}
+                           onChange={this.handleChange}/>
+                    <input className="surname" type="text" placeholder="Wpisz nazwisko" value={this.state.user.surname}
+                           onChange={this.handleChange}/>
+                    <input className="pesel" type="number" placeholder="Wpisz pesel" value={this.state.user.pesel}
+                           onChange={this.handleChange}/>
+                    <input className="mobilePhone" type="number" placeholder="Wpisz numer telefonu komórkowego" value={this.state.user.mobilePhone}
+                           onChange={this.handleChange}/>
+                    <input className="landlinePhone" type="number" placeholder="Wpisz numer telefonu stacjonarnego" value={this.state.user.landlinePhone}
+                           onChange={this.handleChange}/>
+                    <input className="email" type="email" placeholder="Wpisz adres email" value={this.state.user.email}
+                           onChange={this.handleChange}/>
+                </form>
             </div>
         )
     }
