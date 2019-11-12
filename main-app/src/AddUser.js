e10b257e265c176e8cc4ce0b7d725a1cccc9fcc3
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2019-11-12 18:49:04 +0100

    Postepy 12.11.19r godz 18:49

--- main-app/src/AddUser.js
+++ main-app/src/AddUser.js
@@ -14,6 +14,19 @@ class AddUser extends React.Component {
         }
     };
 
+    handleChange = (e) => {
+
+        let newUser;
+
+        newUser = {
+            ...this.state.user,
+            [e.target.name]: e.target.value,
+        };
+        this.setState({
+          user: newUser
+        })
+    };
+
 
 
 
@@ -21,12 +34,18 @@ class AddUser extends React.Component {
         return (
             <div className="addUserForm">
                 <form onSubmit={this.addNewUser}>
-                    <input className="name" type="text" placeholder="Wpisz imię" value={this.state.user.name}/>
-                    <input className="surname" type="text" placeholder="Wpisz nazwisko" value={this.state.user.surname}/>
-                    <input className="pesel" type="number" placeholder="Wpisz pesel" value={this.state.user.pesel}/>
-                    <input className="mobilePhone" type="number" placeholder="Wpisz numer telefonu komórkowego" value={this.state.user.mobilePhone}/>
-                    <input className="landlinePhone" type="number" placeholder="Wpisz numer telefonu stacjonarnego" value={this.state.user.landlinePhone}/>
-                    <input className="email" type="email" placeholder="Wpisz adres email" value={this.state.user.email}/>
+                    <input name="name" type="text" placeholder="Wpisz imię" value={this.state.user.name}
+                        onChange={this.handleChange}/>
+                    <input name="surname" type="text" placeholder="Wpisz nazwisko" value={this.state.user.surname}
+                        onChange={this.handleChange}/>
+                    <input name="pesel" type="number" placeholder="Wpisz pesel" value={this.state.user.pesel}
+                        onChange={this.handleChange}/>
+                    <input name="mobilePhone" type="number" placeholder="Wpisz numer telefonu komórkowego" value={this.state.user.mobilePhone}
+                        onChange={this.handleChange}/>
+                    <input name="landlinePhone" type="number" placeholder="Wpisz numer telefonu stacjonarnego" value={this.state.user.landlinePhone}
+                        onChange={this.handleChange}/>
+                    <input name="email" type="email" placeholder="Wpisz adres email" value={this.state.user.email}
+                        onChange={this.handleChange}/>
                     <button type="submit">Dodaj</button>
                 </form>
             </div>
